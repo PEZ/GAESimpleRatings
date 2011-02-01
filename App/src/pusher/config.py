be92b6229ccdb8bf4586e00fdf5104474a18d96d
Peter Stromberg <pez@pezius.com>
2011-02-01 20:59:14 +0100

    Unignoring app.yaml and config.py

---
+++ App/src/pusher/config.py
@@ -0,0 +1,8 @@
+'''
+Created on Jan 20, 2011
+
+@author: cobpez
+'''
+APP_ID = "cba783e30fc7797224fe563ad75099dbbdf92482"
+APP_SECRET = "6d31ecf713b6ae6f7f1656ccb80dd672c439392b"
+SERVER_URL = "https://pusher.projectplace.com:8888"
